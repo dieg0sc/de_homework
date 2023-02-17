a4e8f367c6eda8180321029b14cbf54db94bd1c6
diegojscacchi@gmail.com <diegojscacchi@gmail.com>
2023-02-17 21:32:51 +0000

    .

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -31,7 +31,7 @@ select
     {{get_payment_type_description(('payment_type'))}} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
-from {{source ('staging', 'green_tripdata') }}
+from {{source ('staging', 'green1_tripdata') }}
 where vendorid is not null 
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
