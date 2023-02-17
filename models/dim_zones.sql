f850507891ae9b515de56bcaa8e72dbd9c605bc7
diegojscacchi@gmail.com <diegojscacchi@gmail.com>
2023-02-17 17:00:01 +0000

    .

--- models/dim_zones.sql
+++ models/dim_zones.sql
@@ -5,5 +5,6 @@ select
     borough,
     zone,
     replace(service_zone, 'Boro', 'Green') as service_zone
+    {# this replacement was done because there were different ways of adressing the zone in the table. #}
 from {{ ref('taxi_zone_lookup') }}
 
