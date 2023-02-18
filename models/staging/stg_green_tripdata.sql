23ecf04f3902002f1522c8c45eefe6e503159af9
diegojscacchi@gmail.com <diegojscacchi@gmail.com>
2023-02-18 19:03:15 +0000

    .

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -3,7 +3,7 @@
 with tripdata as 
 (
   select *,
-    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+    row_number() over(partition by integer(vendorid), lpep_pickup_datetime) as rn
   from {{ source('staging','green1_tripdata') }}
   where vendorid is not null 
 )
